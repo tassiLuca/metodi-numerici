efb4374bf95397deacb3a53adcba2dd686529bfd
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-05-06 18:14:27 +0200

    Added first exercise@

---
+++ 04-sistemi-lineari/es01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 18:11:06 2021
+
+@author: lucatassi
+"""
+
+'''
+Al variare di n = 100, ..., 200 costruire la matrice:
+            A[i, j] = sqrt(2 / (n + 1)) * sin((i + 1) * (j - 1) * pi / (n + 1))
+i, j = 0, ..., n - 1 di dimensione n * n ...
+
+'''
