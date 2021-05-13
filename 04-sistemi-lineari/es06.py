5f64869aca182f40e4f84c457ad411b203a1ac3f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-05-13 15:45:53 +0200

    Finished es06

--- 04-sistemi-lineari/es06.py
+++ 04-sistemi-lineari/es06.py
@@ -16,5 +16,51 @@ x_esatta = (1, 1, ..., 1)^T.
 
 import numpy as np
 import Sistemi_lineari as sl
+import matplotlib.pyplot as plt
 
+start = 1
+stop = 21
 
+n = 100
+v = np.random.rand(n, 1)
+v = v / np.linalg.norm(v, 2)
+Q = np.eye(n) - 2 * np.outer(v, v.T)    # v.T è il vettore trasposto di v
+D = np.eye(n)
+x_esatta = np.ones((n, 1))
+
+indice_condizionamento = []
+err_rel_nopivot = []
+err_rel_pivot = []
+
+for k in range(start, stop):
+    D[n - 1, n - 1] = 10.0 ** k
+    A = np.dot(Q, D)
+    indice_condizionamento.append(np.linalg.cond(A, 2)) # calcolo il numero di condizionamento di A
+    b = np.dot(A, x_esatta)
+    
+    # Senza pivoting 
+    P, L, U, flag = sl.LU_nopivot(A)
+    if flag == 0:
+        x_nopivot, flag = sl.LUsolve(L, U, P, b)
+    else :
+        print("Sistema non risolubile senza strategia pivotale.")
+    
+    err_nopivot = np.linalg.norm(x_nopivot - x_esatta, 2) / np.linalg.norm(x_esatta, 2)
+    err_rel_nopivot.append(err_nopivot)
+    
+    # Con pivoting 
+    P, L, U, flag = sl.LU_pivot(A)
+    if flag == 0:
+        x_pivot, flag = sl.LUsolve(L, U, P, b)
+    else :
+        print("Sistema non risolubile con strategia pivotale parziale.")
+    
+    err_pivot = np.linalg.norm(x_pivot - x_esatta, 2) / np.linalg.norm(x_esatta, 2)
+    err_rel_pivot.append(err_pivot)
+        
+plt.loglog(indice_condizionamento, err_rel_nopivot, 'ro-', 
+           indice_condizionamento, err_rel_pivot, 'bo-')
+plt.legend(['Not pivot', 'Pivot'])
+plt.xlabel('Indice di condizionamento')
+plt.ylabel('Errore relativo sulla soluzione')
+plt.show()
