64126c3b87971399cc81abd0a2f1397339c46ae0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-05-20 21:29:11 +0200

    Added interpolation

--- 06-minimi-quadrati/es02.py
+++ 06-minimi-quadrati/es02.py
@@ -15,4 +15,21 @@ Quale tra le tre approssimazioni risulta migliore?
 Confrontare i grafici e la norma euclidea al quadrato del vettore dei residui. 
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import minimi_quadrati as mq
 
+x = np.array([0.0004, 0.2507, 0.5008, 2.0007, 8.0013])
+y = np.array([0.0007, 0.0162, 0.0288, 0.0309, 0.0310])
+x_val = np.linspace(np.min(x), np.max(x), 100)
+y_val = []
+
+for n in range(1, 4):
+    a = mq.QR(x, y, n)
+    p = np.polyval(a, x_val)
+    plt.plot(x_val, p)
+
+plt.plot(x, y, 'go')
+plt.legend(['Retta di regressione', 'Parabola', 'Cubica'])
+plt.show()
+    
