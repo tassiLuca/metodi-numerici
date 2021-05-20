b58854dc006d8b937aedaa3c86a07543a1de73f2
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-05-20 17:33:17 +0200

    Added es02

---
+++ 06-minimi-quadrati/es02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Esercizio 2
+-------------------
+
+Per i dati (x_i, y_i) riportati nei seguenti array:
+    x = [0.0004, 0.2507, 0.5008, 2.0007, 8.0013];
+    y = [0.0007, 0.0162, 0.0288, 0.0309, 0.0310];
+-- costruire la retta di regressione;
+-- costruire la parabola approssimante i dati nel senso dei minimi quadrati;
+-- costruire la cubica approssimante i dati nel senso dei minimi quadrati;
+
+Quale tra le tre approssimazioni risulta migliore? 
+Confrontare i grafici e la norma euclidea al quadrato del vettore dei residui. 
+"""
+
+
