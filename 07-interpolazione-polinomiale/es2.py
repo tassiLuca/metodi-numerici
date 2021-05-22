d9b8ee96816271dfb5c6bba1e909d4ec6c2f0268
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-05-22 14:56:57 +0200

    added es2

---
+++ 07-interpolazione-polinomiale/es2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Esercizio 2
+-----------
+Realizzare uno script che calcoli nella forma di Lagrange i polinomi che
+interpolano le funzioni test sin(x) e cos(x) nei punti xk = kπ/2, con
+k = 0, 1, 2, 3, 4. Visualizzare graficamente i polinomi ottenuti insieme alle
+funzioni assegnate.
+
+"""
+
