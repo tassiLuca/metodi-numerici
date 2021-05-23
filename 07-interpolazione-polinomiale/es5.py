b35c0a3dae441afc5688922e9e8792ce12a5c5f9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-05-23 10:32:17 +0200

    Added comments

--- 07-interpolazione-polinomiale/es5.py
+++ 07-interpolazione-polinomiale/es5.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 23 10:18:05 2021
-
-@author: lucatassi
+Esercizio 5
+-----------
+Per n = 5, 10, 15, 20 fornire un’approssimazione della costante di Lebesgue scegliendo x_1, x_2 , ..., x_n+1 
+equispaziati in [−1, 1] oppure coincidenti con i nodi di Chebyshev x_i = cos(((2i + 1)π) / (2(n + 1))) i = 0, ..., n
 """
 
