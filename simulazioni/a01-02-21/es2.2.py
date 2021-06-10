b3155936e62d04cbe78e8ad28508302e5bc70214
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-06-10 15:37:16 +0200

    Added es2.2

---
+++ simulazioni/a01-02-21/es2.2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Appello 1 Febbraio 2021 - Esercizio 2
+-------------------------------------
+INTEGRAZIONE ED INTERPOLAZIONE
+"""
+
+import numpy as np
+
+fname = lambda x : 
+
