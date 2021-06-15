1c289dede24b55eeddc90947991693b82b8482d0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-06-15 18:08:54 +0200

    Improved programming

--- simulazioni/a08-09-20/es2.py
+++ simulazioni/a08-09-20/es2.py
@@ -3,23 +3,31 @@
 """
 APPELLO DELL'8-10-20 - ESERCIZIO 2
 ----------------------------------
-INTERPOLAZIONE
+INTERPOLAZIONE POLINOMIALE
 """
 
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def pol_lagrange(nodes, j):
+fname = lambda x : 1 / (1 + 900 * x**2)
+a, b = -1, 1
+
+'''
+a)  si determinano i due polinomi di interpolazione di grado n = 5 : 30 : 5 della funzione f.
+'''
+
+def lagrange_pol(nodes, j):
     zeros = np.zeros_like(nodes)
     
     if j == 0:
         zeros = nodes[1:]
     else:
-        zeros = np.append(nodes[0:j], nodes[j+1:])
-
+        zeros = np.append(nodes[:j], nodes[j+1:])
+    
     num = np.poly(zeros)
     den = np.polyval(num, nodes[j])
+    
     return num / den
 
 def lagrange_intrpl(nodes, ordinates, points):
@@ -28,81 +36,88 @@ def lagrange_intrpl(nodes, ordinates, points):
     L = np.zeros((n, m))
     
     for k in range(n):
-        k_pol = pol_lagrange(nodes, k)
+        k_pol = lagrange_pol(nodes, k)
         L[k, :] = np.polyval(k_pol, points)
-    
+        
     return np.dot(ordinates, L)
 
-def chebychev_nodes(a, b, n):
+def chebyshev_nodes(a, b, n):
     t1 = (a + b) / 2
     t2 = (b - a) / 2
-    nodes = np.zeros((n + 1,))
+    x = np.zeros((n + 1, ))
     for i in range(n + 1):
-        nodes[i] = t1 + t2 * np.cos(((2 * i + 1) * math.pi) / (2 * (n + 1)))
+        x[i] = t1 + t2 * np.cos(((2 * i + 1) * math.pi) / (2 * (n + 1)))
+    return x
+
+'''
+b)  dopo aver creato la Figura 1 e suddiviso la finestra grafica in 3 × 2 sottofinestre, si disegnano nelle 
+    6 sottofinestre i grafici degli errori del polinomio interpolatorio su nodi equispaziati al variare di n.
+c)  dopo aver creato la Figura 1 e suddiviso la finestra grafica in 3 × 2 sottofinestre, si disegnano nelle 
+    6 sottofinestre i grafici degli errori del polinomio interpolatorio sui nodi chebyshev al variare di n.
+d)  si calcolano le approssimazioni della costante di Lebesgue sia nel caso di nodi equispaziati che di Chebyshev,
+    e si rappresentano in un grafico in scala semilogaritmica su y al variare di n (Figura 3).
+'''   
+
+def lebesgue(nodes, points):
+    lebesgue_acc = np.zeros_like((points, ))
+    
+    for k in range(nodes.size):
+        k_pol = lagrange_pol(nodes, k)
+        lebesgue_acc = lebesgue_acc + np.abs(np.polyval(k_pol, points))
         
-    return nodes
+    return np.max(lebesgue_acc)
 
-fname = lambda x : 1 / (1 + 900 * x**2)
-a = -1
-b = +1
+start, stop, step = 5, 30, 5
+points = np.linspace(a, b, 100)
 
-points = np.linspace(a, b, 200)
+size = int((stop + step - start) / step)
+equi_lebesgue = np.zeros((size, ))
+cheb_lebesgue = np.zeros((size, ))
 
-equi_lebesgue = np.zeros(6)
-cheb_lebesgue = np.zeros(6)
-equi_lebesgue_acc = np.zeros(200)
-cheb_lebesgue_acc = np.zeros(200)
-
-fig = 1
-i = 0
-for n in range(5, 31, 5):
-    # nodi equispaziati
+i = 1
+for n in range(start, stop + step, step):
+    # calcolo polinomio interpolante su nodi equispaziati
     equi_nodes = np.linspace(a, b, n + 1)
     equi_ordinates = fname(equi_nodes)
     equi_pol = lagrange_intrpl(equi_nodes, equi_ordinates, points)
-    # calcolo errore
-    equi_err = abs(fname(points) - equi_pol)
-    # calcolo lebesgue
-    for k in range(n + 1):
-        pol = pol_lagrange(equi_nodes, k)
-        equi_lebsgue_acc = equi_lebesgue_acc + np.abs(np.polyval(pol, points))
-    equi_lebesgue[i] = np.max(equi_lebsgue_acc)    
+    # calcolo errore 
+    equi_err = np.abs(fname(points) - equi_pol)
+    # calcolo costante di lebesgue
+    equi_lebesgue[i - 1] = lebesgue(equi_nodes, points)
     
-    plt.subplots_adjust(hspace = 0.7, wspace = 0.3)
-    plt.subplot(3, 2, fig)
-    plt.title("n = " + str(n))
+    # grafico gli errori
+    plt.subplot(3, 2, i)
+    plt.subplots_adjust(left = 0, right = 1, wspace = 0.3, hspace = 0.5)
     plt.plot(points, equi_err)
-    fig += 1
+    plt.legend(["n =" + str(n)])
     i += 1
     
-plt.show()  
-
-fig = 1
-i = 0
-for n in range(5, 31, 5):
-    # nodi di Chebychev
-    cheb_nodes = chebychev_nodes(a, b, n)
-    cheb_ordinates = fname(cheb_nodes)
-    cheb_pol = lagrange_intrpl(cheb_nodes, cheb_ordinates, points)
-    # calcolo errore
-    cheb_err = abs(fname(points) - cheb_pol)
-    # calcolo lebesgue
-    for k in range(n + 1):
-        pol = pol_lagrange(cheb_nodes, k)
-        cheb_lebsgue_acc = cheb_lebesgue_acc + np.abs(np.polyval(pol, points))
-    cheb_lebesgue[i] = np.max(cheb_lebsgue_acc)
-    
-    plt.subplots_adjust(hspace = 0.7, wspace = 0.3)
-    plt.subplot(3, 2, fig)
-    plt.plot(points, cheb_err)
-    plt.title("n = " + str(n))
-    fig += 1
-    i += 1
-   
 plt.show()
 
-# grafico costanti di lebegue
-plt.semilogy(range(5, 31, 5), equi_lebesgue, 'o-', range(5, 31, 5), cheb_lebesgue, '*-')
-plt.legend(["Nodi equispaziati", "Nodi di Chebychev"])
-plt.legend()
+
+i = 1
+for n in range(start, stop + step, step):
+    # calcolo polinomio interpolante su nodi di chebyshev
+    cheb_nodes = chebyshev_nodes(a, b, n)
+    cheb_ordinates = fname(cheb_nodes)
+    cheb_pol = lagrange_intrpl(cheb_nodes, cheb_ordinates, points)
+    # calcolo errore 
+    cheb_err = np.abs(fname(points) - cheb_pol)
+    # calcolo costante di lebesgue
+    cheb_lebesgue[i - 1] = lebesgue(cheb_nodes, points)
+    
+    # grafico gli errori
+    plt.subplot(3, 2, i)
+    plt.subplots_adjust(left = 0, right = 1, wspace = 0.3, hspace = 0.5)
+    plt.plot(points, cheb_err)
+    plt.legend(["n =" + str(n)])
+    i += 1
+
+plt.show()
+
+# grafico le costanti di Lebesgue
+n = np.arange(start, stop + step, step)
+plt.semilogy(n, cheb_lebesgue, '-o', n, equi_lebesgue, '-o')
+plt.legend(["Chebyshev", "Equi"])
+plt.grid(True)
 plt.show()
